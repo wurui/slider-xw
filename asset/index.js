f28d5b54bcfe4716848c729eb5799714f3cbe783
wurui <wurui.wr@gmail.com>
2018-11-29 16:24:04 +0800

    fix bug

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,109 @@
 define([],function(){
+
+  
+  var Scroll={
+  	timer:null,
+  	
+  	to:function(el,sl,fn){
+  		var delta=el.scrollLeft-sl;
+  		//console.log(delta)
+  		if(delta){
+  			el.scrollLeft-= delta>0?Math.ceil(delta/2):Math.floor(delta/2)
+  		
+  		}else{
+  			this.stop();
+  			return fn && fn();
+  		} 
+  		var _this=this;
+  		this.timer=setTimeout(function(){
+  			_this.to(el,sl,fn)
+  		},60)
+  	},
+  	stop:function(){
+  		if(this.timer){
+  			clearTimeout(this.timer);
+  			this.timer=null;
+  		}
+  	}
+  };
   return {
     init:function($mod){
+    	
+    	this.initScroll($('.J_win',$mod),$('.J_index',$mod));
+    	
 
+    	
+    },
+    initScroll:function($win,$index){
+    	var count=$win.attr('data-count')-0;
+    	var lastScrollLeft=0;
+    	var checkScroll=function(){
+    		var win=$win[0];
+    		var sw=win.scrollWidth,
+    		cw=win.clientWidth,
+    		sl=win.scrollLeft,
+    		index=0;
+    		
+    		if(lastScrollLeft<sl){
+    			index=Math.ceil(sl/cw);
+    		}else{
+    			index=Math.floor(sl/cw);
+    		}
+    		
+    		Scroll.to(win,index * cw,function(){
+    			//console.log('done')
+    			lastScrollLeft=win.scrollLeft;
+    			$index.attr('data-on',index+1)
+    		})
+    		
+
+    	};
+    	var AutoScroll={
+    		stop:function(){
+    			this.IV && clearInterval(this.IV)
+    		},
+    		goNext:function(){
+    			var win=$win[0];
+	    		var sw=win.scrollWidth,
+	    		cw=win.clientWidth;
+	    		
+	    		if(win.scrollLeft==sw-cw){
+	    			Scroll.to(win,0,function(){
+		    			//console.log('done')
+		    			lastScrollLeft=win.scrollLeft;
+		    			$index.attr('data-on',1)
+		    		})
+	    		}else{
+	    			win.scrollLeft+=1;
+	    		}
+    		},
+    		start:function(){
+    			var me=this;
+    			this.IV=setInterval(me.goNext,3000)
+    		}
+    		
+    	};
+    	
+    	var TO;
+    	$win.on('touchstart',function(){
+    		Scroll.stop();
+    		AutoScroll.stop()
+    	}).on('touchend',function(){
+    		
+    		AutoScroll.start()
+    	}).on('scroll',function(){
+    		//console.log('1');
+    		if(!Scroll.timer){
+    			if(TO){
+	    			clearTimeout(TO)
+	    		}
+	    		TO=setTimeout(checkScroll,200)
+    		}
+    		
+    	});
+    	checkScroll();
+    	AutoScroll.start()
     }
+
   }
 })
--- mod.xsl
+++ mod.xsl
@@ -2,9 +2,32 @@
     <xsl:template match="/root" name="wurui.slider-xw">
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-slider-xw" ox-mod="slider-xw">
-            <h1>
-                This is mod slider-xw;
-            </h1>
+        	<xsl:variable select="data/ui-imglist/i[position() &lt; 7]" name="list"/>
+        	<xsl:variable select="count($list)" name="len"/>
+
+            <div class="J_win window" data-count="{$len}">
+            	<table class="sliderbox" cellpadding="0" cellspacing="0" width="{$len}00%">
+            		<tbody>
+            			<tr>
+		            		<xsl:for-each select="$list">
+		            			<td class="item" width="{100 div $len}00%">
+		            				<a href="">
+			            				<img class="mainpic" style="background-image:url({img})" src="//a.oxm1.cc/img/blank.png"/>
+			            				<p class="text-box">
+			            					<xsl:value-of select="title"/>
+			            				</p>
+			            			</a>
+		            			</td>
+		            		</xsl:for-each>
+	            		</tr>
+	            	</tbody>
+            	</table>
+            </div>
+            <div class="J_index index" data-on="1">
+            	<xsl:for-each select="$list">
+            		<i></i>
+            	</xsl:for-each>
+            </div>
         </div>
     </xsl:template>
 </xsl:stylesheet>
